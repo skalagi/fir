9c21af1fdf0cd577ba3662a0b609a63bad9d8710
damian <carftjr@gmail.com>
2017-12-02 17:01:21 +0100

    I should think about tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,9 +8,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
 });
