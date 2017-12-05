8e7585542d4cbe4527c192ab3f6ebaaaa245adc7
damian <carftjr@gmail.com>
2017-12-05 18:31:44 +0100

    error handling

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 import { Subject } from 'rxjs';
 
 @Injectable()
 export class Socket {
   public messages: Subject<any> = new Subject();
 
+  constructor(private snack: MatSnackBar) {
+    this.messages.filter(({ error }) => error)
+      .subscribe(({ error }) => {
+        snack.open(error, 'rozumiem'); //nicer error emblems
+      });
+  }
+
   public send(msg: any) {
     if (msg.controller === 'ChangeState') {
       this.messages.next({ ...msg, controllerResponse: 'OK' });
     }
 
-    console.log(msg);
+    this.snack.open(msg.controller, null, { duration: 700 });
   }
 }
