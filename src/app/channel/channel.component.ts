318321afcf811ebe4515bf25b9bea28c7b441030
damian <carftjr@gmail.com>
2017-12-05 18:00:51 +0100

    channel sending state and responding to response

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -1,1 +1,14 @@
-import { Component, OnInit } from '@angular/core';  @Component({   selector: 'channel',   templateUrl: './channel.component.html',   styleUrls: ['./channel.component.scss'] }) export class ChannelComponent {   private identity;      public label = 'wesolych swiat';   public color = 'primary';    toggleColor() {     this.color = this.color === ''       ? 'primary' : '';   } } 
+import { Component, OnInit, Input } from '@angular/core';  import { Socket } from "../socket";
+
+@Component({   selector: 'channel',   templateUrl: './channel.component.html',   styleUrls: ['./channel.component.scss'] }) export class ChannelComponent {   constructor(private socket: Socket) {}   private state: boolean = false;    @Input()   private identity: number;      public label: string = 'wesolych swiat';    ngOnInit() {
+    this.socket.messages.subscribe(ev => {
+      if (ev.controller === 'ChangeState'
+        && ev.controllerResponse === 'OK'
+        && ev.value.identity === this.identity
+        ) {
+        this.state = ev.value.state;
+      }
+    });
+  }    toggleColor() {     const { identity, state } = this;      this.socket.send({       controller: 'ChangeState',       value: {         state: !state,
+        identity
+      }     });   } } 
