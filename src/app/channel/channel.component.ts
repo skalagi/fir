db4da11a466b2faea05b984ce42877632db55eb9
damian <carftjr@gmail.com>
2017-12-05 18:10:35 +0100

    reciving change from outside app

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -1,14 +1,1 @@
-import { Component, OnInit, Input } from '@angular/core';  import { Socket } from "../socket";
-
-@Component({   selector: 'channel',   templateUrl: './channel.component.html',   styleUrls: ['./channel.component.scss'] }) export class ChannelComponent {   constructor(private socket: Socket) {}   private state: boolean = false;    @Input()   private identity: number;      public label: string = 'wesolych swiat';    ngOnInit() {
-    this.socket.messages.subscribe(ev => {
-      if (ev.controller === 'ChangeState'
-        && ev.controllerResponse === 'OK'
-        && ev.value.identity === this.identity
-        ) {
-        this.state = ev.value.state;
-      }
-    });
-  }    toggleColor() {     const { identity, state } = this;      this.socket.send({       controller: 'ChangeState',       value: {         state: !state,
-        identity
-      }     });   } } 
+import { Component, OnInit, Input } from '@angular/core';  import { Socket } from "../socket";  @Component({   selector: 'channel',   templateUrl: './channel.component.html',   styleUrls: ['./channel.component.scss'] }) export class ChannelComponent {   constructor(private socket: Socket) {}   private state: boolean = false;    @Input()   private identity: number;      public label: string = 'wesolych swiat';    ngOnInit() {     this.socket.messages       .filter(({ controllerResponse }) => controllerResponse ? controllerResponse === 'OK' : true)       .filter(({ controller }) => controller === 'ChangeState')       .filter(({ value }) => value.identity === this.identity)       .subscribe(({ controllerResponse, value }) => this.state = value.state);   }    toggleColor() {     const { identity, state } = this;      this.socket.send({       controller: 'ChangeState',       value: {         state: !state,         identity       }     });   } } 
