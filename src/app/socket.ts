a87ad53d4a74987a632d0b9711a955455d8e15a4
damian <carftjr@gmail.com>
2017-12-06 22:55:18 +0100

    check socket endpoint

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material';
 import { Subject } from 'rxjs';
+
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class Socket {
   public messages: Subject<any> = new Subject();
 
-  constructor(private snack: MatSnackBar) {
+  constructor(private snack: MatSnackBar, http: HttpClient) {
+    http.get(`${environment.uri}/getEndpoint`).subscribe(socketUri => {
+      console.log(socketUri);
+    });
+
     this.messages.filter(({ error }) => error)
       .subscribe(({ error }) => {
         snack.open(error, 'rozumiem'); //nicer error emblems
