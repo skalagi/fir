0d221dddea7c2592b59661eebe5c4417ec9186fb
damian <carftjr@gmail.com>
2017-12-21 03:18:47 +0100

    disableable channel

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';  import { Socket } from "../socket";  import { ActionService } from '../action.service';
 
-@Component({   selector: 'channel',   templateUrl: './channel.component.html',   styleUrls: ['./channel.component.scss'] }) export class ChannelComponent {   constructor(private actions: ActionService) {}   @Input() public label: string = '';   @Input() public identity: number;   public state: boolean = false;    ngOnInit() {     this.actions.action('ChangeState', (value) => {       if (value.identity === this.identity) {
+@Component({   selector: 'channel',   templateUrl: './channel.component.html',   styleUrls: ['./channel.component.scss'] }) export class ChannelComponent {   constructor(private actions: ActionService) {}   @Input() public label: string = '';   @Input() public identity: number;   @Input() public active: boolean;   public state: boolean = false;    ngOnInit() {     this.actions.action('ChangeState', (value) => {       if (value.identity === this.identity) {
         this.state = value.state;
       }     });   }    toggleColor() {     const { identity, state } = this;      this.actions.change({       controller: 'ChangeState',       value: {         state: !state,         identity       }     });   } } 
