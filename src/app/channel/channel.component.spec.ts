02274b949b282e530fd702c85b0dc4b5faedd7e7
damian <carftjr@gmail.com>
2017-12-21 02:18:23 +0100

    adapt channel to requirments

--- src/app/channel/channel.component.spec.ts
+++ src/app/channel/channel.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+  import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
