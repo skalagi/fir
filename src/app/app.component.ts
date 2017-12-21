a085c97947c629b0666ceb93d41d7c7eb91bf3cd
damian <carftjr@gmail.com>
2017-12-21 02:18:57 +0100

    app component get channels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'root',
@@ -6,4 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public sequences: boolean = false;
+  public channels: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {     this.http.get(`${ environment.uri }/getChannels`).subscribe((channels: any[]) => this.channels = channels);
+}
 }
