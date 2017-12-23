8a3df8d723b2d3ad23e100f9cd2e2a3b205ece26
damian <carftjr@gmail.com>
2017-12-23 17:53:26 +0100

    humanreadable color names

--- src/app/sequencer/sequencer.component.ts
+++ src/app/sequencer/sequencer.component.ts
@@ -1,6 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormArray, FormGroup } from '@angular/forms';
 import { ActionService } from '../action.service';
+import * as colors from 'color-name';
+
+function hexToRgb(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
 
 export interface Color {
   delay: number;
@@ -17,6 +34,7 @@ export class SequencerComponent implements OnInit {
   @Input() active;
   public colors: Color[] = [];
   public currentColor: string;
+  public colorName: string;
 
   private color(hex) {
     return { hex, delay: null };
@@ -44,6 +62,7 @@ export class SequencerComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.addColor();     this.actions.action('ChangeColor', (color) => {       this.currentColor = color.hex;     });
+    this.addColor();     this.actions.action('ChangeColor', (color) => {       const rgb = hexToRgb(color.hex);       let colorName;        Object.keys(colors).filter(color => {         const col = colors[color];          if (col[0] == rgb.r && col[1] == rgb.g && col[2] == rgb.b) {           colorName = color;
+        }       });        this.colorName = colorName;       this.currentColor = color.hex;     });
   }
 }
