a1b5a46b639e78871b79239c5e99bf586e8ec91b
damian <carftjr@gmail.com>
2017-12-23 17:30:43 +0100

    toggle state only when toggling is active

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -2,4 +2,4 @@ import { Component, OnInit, Input } from '@angular/core';  import { Socket }
 
 @Component({   selector: 'channel',   templateUrl: './channel.component.html',   styleUrls: ['./channel.component.scss'] }) export class ChannelComponent {   constructor(private actions: ActionService) {}   @Input() public label: string = '';   @Input() public identity: number;   @Input() public active: boolean;   public state: boolean = false;    ngOnInit() {     this.actions.action('ChangeState', (value) => {       if (value.identity === this.identity) {
         this.state = value.state;
-      }     });   }    toggleColor() {     const { identity, state } = this;      this.actions.change({       controller: 'ChangeState',       value: {         state: !state,         identity       }     });   } } 
+      }     });   }    toggleColor() {     const { identity, state, active } = this;      if (!active) return;      this.actions.change({       controller: 'ChangeState',       value: {         state: !state,         identity       }     });   } } 
