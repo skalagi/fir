c4956e29154cb07a183e90d7d7a30ff53071cd37
damian <carftjr@gmail.com>
2017-12-24 15:53:45 +0100

    screen setingup provided stream

--- src/app/screen/screen.component.ts
+++ src/app/screen/screen.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'screen',
@@ -6,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./screen.component.scss']
 })
 export class ScreenComponent implements OnInit {
+  public youtubeId;
 
-  constructor() { }
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
+
+  public youtubeUrl() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.youtubeId}?autoplay=1`);
+  }
 
   ngOnInit() {
+    this.http.get(environment.uri + '/getStream').subscribe((stream: any) => {
+      if (stream.type == 'yt') {
+        this.youtubeId = stream.value;
+      }
+    });
   }
 }
--- src/app/screen/screen.module.ts
+++ src/app/screen/screen.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ScreenComponent } from './screen.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
   ],
   declarations: [ScreenComponent],
   exports: [ScreenComponent],
