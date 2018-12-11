0b0f989f191345aa0a37ce0d150488ca518d82c4
Damian <martyniak.innovative@gmail.com>
2018-12-11 23:17:21 +0100

    update main fir structure

--- src/app/fir/fir.component.ts
+++ src/app/fir/fir.component.ts
@@ -1,13 +1,12 @@
 import { Component, HostBinding, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { ObservableMedia } from '@angular/flex-layout';
-import { MatSidenav, MatDrawer } from '@angular/material';
+import { MatDrawer } from '@angular/material/sidenav';
 import { take } from 'rxjs/operators';
 import * as colors from 'color-name';
 
-import { environment } from '../../environments/environment';
 import { Socket } from '../action/socket';
 import { ActionService } from '../action/action.service';
+import { ChannelsQuery } from '../channel/state';
 
 @Component({
   selector: 'fir',
@@ -18,7 +17,7 @@ export class FirComponent {
   public sequences = false;
   public live = true;
   public networkStatus = 'finding way to the house';
-  public channels: any[] = [];
+  public channels$;
   public drawerMode = 'side';
   public currentColor: string;
   public colorName: string;
@@ -30,7 +29,12 @@ export class FirComponent {
 
   @ViewChild(MatDrawer) public drawer: MatDrawer;
 
-  constructor(private http: HttpClient, private media: ObservableMedia, private socket: Socket, private actions: ActionService) { }
+  constructor(
+    private channelQ: ChannelsQuery,
+    private media: ObservableMedia,
+    private socket: Socket,
+    private actions: ActionService
+  ) { }
 
   ngAfterViewInit() {
     this.socket.message$.pipe(take(1)).subscribe(() => {
@@ -38,12 +42,27 @@ export class FirComponent {
     });
   }
 
-  ngOnInit() {     this.http.get(`${environment.uri}/getChannels`).subscribe((channels: any[]) => this.channels = channels);
-     this.actions.action('ChangeColor', ({ rgb }) => {       let colorName;        Object.keys(colors).filter(color => {         const col = colors[color];          if (col[0] == rgb[0] && col[1] == rgb[1] && col[2] == rgb[2]) {           colorName = color;
-        }       });        this.colorName = colorName;       this.currentColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;     });
+  ngOnInit() {
+    this.channels$ = this.channelQ.all$;
+
+    this.actions.action('ChangeColor', ({ rgb }) => {
+      let colorName;
+
+      Object.keys(colors).filter(color => {
+        const col = colors[color];
+
+        if (col[0] == rgb[0] && col[1] == rgb[1] && col[2] == rgb[2]) {
+          colorName = color;
+        }
+      }); 
+
+      this.currentColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+      this.colorName = colorName;
+    });
 
     this.media.subscribe(({ mqAlias }) => {
       this.columns = mqAlias == 'xs' ? 2 : 4;
+
       if (mqAlias == 'sm' || mqAlias == 'xs') {
         this.drawerMode = 'push';
         this.hostWidth = '100%';
@@ -54,5 +73,5 @@ export class FirComponent {
         this.hostWidth = '';
       }
     });
-}
+  }
 }
