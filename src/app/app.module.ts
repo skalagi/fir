230c8c3efce82de3dcba01fc522670deb647f44c
Damian <martyniak.innovative@gmail.com>
2018-12-15 00:19:08 +0100

    create new root app (routing) module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,28 @@ import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { environment } from '../environments/environment';
-import { FirComponent } from './fir/fir.component';
 import { FirModule } from './fir/fir.module';
 import { akitaDevtools } from '@datorama/akita';
-import { ChannelsComponent } from './channels/channels.component';
+import { AppComponent } from './app/app.component';
+
+import { RouterModule } from '@angular/router';
 
 if (!environment.production) {
   akitaDevtools();
 }
 
+const routes = [
+  { path: '', loadChildren: './pages/homepage/homepage.module#HomepageModule' },
+];
+
 @NgModule({
   imports: [
     BrowserModule,
     FirModule,
+    RouterModule.forRoot(routes),
     environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
   ],
-  bootstrap: [FirComponent],
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
 })
 export class AppModule { }
