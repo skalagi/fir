442e297ae150982b73062ec0813b0c145df3f2f3
Damian <martyniak.innovative@gmail.com>
2018-12-14 19:03:17 +0100

    remake queue-preview

--- src/app/queue/queue.module.ts
+++ src/app/queue/queue.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QueuePreviewComponent } from '../queue-preview/queue-preview.component';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [QueuePreviewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
   ],
   exports: [QueuePreviewComponent],
 })
