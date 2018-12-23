a8342f19706952cdc5018719b93910c46a7b18dc
Damian <martyniak.innovative@gmail.com>
2018-12-23 20:54:53 +0100

    refactor homepage

--- src/app/pages/homepage/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage/homepage.component.ts
@@ -1,31 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
-import { ChannelsQuery } from '../../../service/channel';
-import { QueueQuery } from '../../../service/queue';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
-  channels$;
-  queue$;
+export class HomepageComponent {
+  channels = [];
+  queue = [];
 
-  queue = [
+  queueDescription = [
     { icon: 'radio_button_unchecked', description: 'turn off lights part' },
     { icon: 'radio_button_checked', description: 'turn on lights part' },
     { icon: 'palette', description: 'led ring color change' },
   ];
-
-  constructor(
-    private channelQ: ChannelsQuery,
-    private queueQ: QueueQuery,
-  ) { }
-
-  ngOnInit() {
-    this.channels$ = this.channelQ.all$;
-    this.queue$ = this.queueQ.show$;
-  }
-
 }
