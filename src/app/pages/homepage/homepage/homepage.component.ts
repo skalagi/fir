1174599e5d921e0c61d1159c3178f6facde4d958
Damian <martyniak.innovative@gmail.com>
2018-12-23 23:49:37 +0100

    add homepage queue elements

--- src/app/pages/homepage/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage/homepage.component.ts
@@ -12,7 +12,18 @@ export class HomepageComponent {
     { id: 2, state: true },
   ];
 
-  queue = [{}, {}, {}, {}, {}, {}];
+  queue = [
+    { r: 255, g: 0, b: 0 },
+    { channel: 0 },
+    { r: 0, g: 255, b: 0 },
+    { channel: 1 },
+    { channel: 2, state: true },
+    { r: 0, g: 255, b: 0 },
+    { channel: 1, state: true },
+    { r: 0, g: 0, b: 255 },
+    { r: 0, g: 255, b: 0 },
+    { channel: 2, state: true },
+  ];
 
   queueDescription = [
     { icon: 'radio_button_unchecked', description: 'turn off lights part' },
