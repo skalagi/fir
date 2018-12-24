768a7ee813e5fcdbdd35d9c619ce5e4d1eb9e1b9
Damian <martyniak.innovative@gmail.com>
2018-12-24 21:25:08 +0100

    add GA

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private router: Router) {}
+  
+  ngOnInit() {
+
+  if (environment.production) {
+    (<any>window).ga('create', 'UA-114939227-1', 'auto');
+    (<any>window).ga('set', 'page', this.router.url);
+    (<any>window).ga('send', 'pageview');
+  }
+
+  this.router.events.subscribe(ev => {
+    if (ev instanceof NavigationEnd) {
+      if (environment.production) {
+        (<any>window).ga('set', 'page', ev.urlAfterRedirects);
+        (<any>window).ga('send', 'pageview');
+      }
+    }
+  });
+  }
+
+}
